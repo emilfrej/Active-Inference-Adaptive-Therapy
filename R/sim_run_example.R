cc0cba9d00c107b6fefde81e314d515f90b38ca3
emil.frej.brunbjerg@gmail.com <code --wait>
2024-05-19 12:09:44 +0200

    made overall policy plots

--- R/sim_run_example.R
+++ R/sim_run_example.R
@@ -7,6 +7,10 @@ treatment_shading <- single_trial_full_data %>%
   mutate(time_plus_1 = timestep + 1,
          timestep_minus_1 = timestep - 1)
 
+#add column for testing
+testing_times <- single_trial_full_data %>%
+  filter(testing_state == "testing")
+  
 
 plot <- ggplot(single_trial_full_data, aes(x = timestep, y = tumor_state)) +
   
@@ -17,7 +21,7 @@ plot <- ggplot(single_trial_full_data, aes(x = timestep, y = tumor_state)) +
   geom_line(aes(y = resistance_state, color = "Resistance Level" ), linetype = 2) +
   
   #add shaded areas for treatment on
-  geom_rect(data = treatment_shading, aes(xmin = timestep_minus_1, xmax = timestep,  ymin = -Inf, ymax = Inf, fill = "Treatment Is Given"), inherit.aes = FALSE, , alpha = 0.4, label = "Treatment is given")  +
+  geom_rect(data = treatment_shading, aes(xmin = timestep_minus_1, xmax = timestep,  ymin = -Inf, ymax = Inf, fill = "Treating"), inherit.aes = FALSE, , alpha = 0.4, label = "Treatment is given")  +
   theme(legend.position = "bottom") +
   guides(fill = guide_legend(title = NULL),
          colour = guide_legend(title = NULL)) +
@@ -25,8 +29,17 @@ plot <- ggplot(single_trial_full_data, aes(x = timestep, y = tumor_state)) +
   #force the resistance level color to black
   scale_color_manual(values = c("black", "red")) +
   
+  #overlay testing points
+  geom_point(data = testing_times, aes(x = timestep, y = 0, shape = "Testing"), size = 1) +
+  
+  #set other x shape for testing
+  scale_shape_manual(values = c("Testing" = 4)) +
+  
+  #remove the shape legend title
+  guides(shape = guide_legend(title = NULL)) +
   
   
+ 
   labs(title = "Tumor and Resistance Level Through Time",
        x = "Time",
        y = "Tumor and Resistance State") 
