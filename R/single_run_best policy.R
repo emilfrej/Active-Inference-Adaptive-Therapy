9ce206261416736a0b6755a553ac70cc031cbf05
emil.frej.brunbjerg@gmail.com <code --wait>
2024-05-21 10:15:49 +0200

    writing on future research

--- R/single_run_best policy.R
+++ R/single_run_best policy.R
@@ -5,10 +5,29 @@ num_policies <- single_trial_full_data$policy[1] %>%
 #create a vector with column names pol 1 pol 2 up pol 6
 pol_names <- paste0("pol_", 1:num_policies)
 
+#chose interesting timesteps
 chosen_timesteps <- c(16, 34)
 
 #construct a dataframe for fep values at a few timesteps 
-fep_ex_df <- fep_ex_df %>% 
+fep_ex_df_full <- single_trial_full_data %>% 
+  #make as many columns for policies
+  separate(policy, into = paste0("pol_at_time", 1:6), sep = "\n", fill = "right") %>%
+  
+  #pivot to long form
+  pivot_longer(cols = c(contains("pol_at_time")), values_to = "pol_at_time", names_to = "future_timepoint") %>% 
+  
+  #calculate actual timepoint by first extract the digit in the future_timepoint column
+  mutate(future_timepoint = as.numeric(str_extract(future_timepoint, "[0-9]+")) + timestep,
+         #remove everything that isnt digits from pol at time
+         pol_at_time = str_remove_all(pol_at_time, "[^0-9]")) %>% 
+  
+  #add coloumn for treating at the each time by extracting the first index of the pol at time and for testing
+  mutate(treatment_planned = ifelse(str_sub(pol_at_time, 1, 1) == "1", 1, 0),
+         testing_planned = ifelse(str_sub(pol_at_time, 5, 5) == "1", 1, 0))
+
+
+#construct a dataframe for fep values at a few timesteps 
+fep_ex_df <- fep_ex_df_full %>% 
   filter(timestep %in% chosen_timesteps)
  
 
--- R/single_run_overall_fep_policies.R
+++ R/single_run_overall_fep_policies.R
@@ -5,23 +5,6 @@ num_policies <- single_trial_full_data$policy[1] %>%
 #create a vector with column names pol 1 pol 2 up pol 6
 pol_names <- paste0("pol_", 1:num_policies)
 
-#construct a dataframe for fep values at a few timesteps 
-fep_ex_df <- single_trial_full_data %>% 
-  #make as many columns for policies
-  separate(policy, into = paste0("pol_at_time", 1:6), sep = "\n", fill = "right") %>%
-  
-  #pivot to long form
-  pivot_longer(cols = c(contains("pol_at_time")), values_to = "pol_at_time", names_to = "future_timepoint") %>% 
-  
-  #calculate actual timepoint by first extract the digit in the future_timepoint column
-  mutate(future_timepoint = as.numeric(str_extract(future_timepoint, "[0-9]+")) + timestep,
-         #remove everything that isnt digits from pol at time
-         pol_at_time = str_remove_all(pol_at_time, "[^0-9]")) %>% 
-  
-  #add coloumn for treating at the each time by extracting the first index of the pol at time and for testing
-  mutate(treatment_planned = ifelse(str_sub(pol_at_time, 1, 1) == "1", 1, 0),
-         testing_planned = ifelse(str_sub(pol_at_time, 5, 5) == "1", 1, 0))
-
 source("sim_run_example.R") 
 
 plot <- ggplot(single_trial_full_data, aes(x = timestep, y = tumor_state)) +
@@ -33,7 +16,7 @@ plot <- ggplot(single_trial_full_data, aes(x = timestep, y = tumor_state)) +
   geom_line(aes(y = resistance_state, color = "Resistance Level" ), linetype = 2) +
   
   #add expected testing
-  geom_point(data = fep_ex_df %>% filter(testing_planned == 1), aes(x = future_timepoint, y = 0, shape = "Planned Testing"), size = 1, alpha = .4) +
+  geom_point(data = fep_ex_df_full %>% filter(testing_planned == 1), aes(x = future_timepoint, y = 0, shape = "Planned Testing"), size = 1, alpha = .4) +
   scale_shape_manual(values = c("Planned Testing" = 4)) +
   
 
@@ -41,7 +24,7 @@ plot <- ggplot(single_trial_full_data, aes(x = timestep, y = tumor_state)) +
   scale_color_manual(values = c("black", "red")) +
   
   #add shaded areas for planned treatement
-  geom_rect(data = fep_ex_df %>% filter(treatment_planned == 1), aes(xmin = future_timepoint, xmax = future_timepoint+1,  ymin = -Inf, ymax = Inf, fill = "Planned Treament"), inherit.aes = FALSE, , alpha = 0.2, label = "Planned Treatment")  +
+  geom_rect(data = fep_ex_df_full %>% filter(treatment_planned == 1), aes(xmin = future_timepoint, xmax = future_timepoint+1,  ymin = -Inf, ymax = Inf, fill = "Planned Treament"), inherit.aes = FALSE, , alpha = 0.2, label = "Planned Treatment")  +
   theme(legend.position = "bottom") +
   guides(fill = guide_legend(title = NULL),
          colour = guide_legend(title = NULL)) +
